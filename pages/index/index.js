fc1b31b4fdb9a97b0c2fa6df086ed55e54378c88
lwz7512 <lwz7512@gmail.com>
2017-12-14 17:02:28 +0800

    remote data access...

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,54 @@
 var base64 = require("../images/base64");
 
 Page({
+
+  data: {
+    loading: true,
+    results: []
+  },
+
   onLoad: function () {
-    this.setData({
-      icon20: base64.icon20,
-      icon60: base64.icon60
-    });
+    var that = this;
 
     wx.setNavigationBarTitle({
       title: '行业报告'
-    })
-  }
+    });
+
+    wx.showShareMenu({
+      withShareTicket: true
+    });
+
+    wx.request({
+      url: 'https://ipintu.com/blog/posts',
+      success: function(res) {
+        console.log(res.data);
+        that.setData({loading: false, results: res.data.posts});
+      },
+      fail: function(res) {},
+      complete: function(res) {},
+    });
+
+
+
+  },
+
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '程序猿情报',
+      path: 'pages/index/index',
+      // path: '/page/user?id=123',
+      success: function(res) {
+        // 转发成功
+      },
+      fail: function(res) {
+        // 转发失败
+      }
+    }
+  },
+
+
 });
