4d5d7848946dec663b39ee19ec86bdc837a63d8b
lwz7512 <lwz7512@gmail.com>
2018-06-05 09:35:28 +0800

    add cards ...

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,9 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
+const Session = require('../../session');
+const config = require('../../config');
+const util = require('../../utils/util');
 
 Page({
   data: {
@@ -9,48 +12,106 @@ Page({
     // hasUserInfo: false,
     imgUrls: [
       '../../images/header.png',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      'https://i.imgur.com/UYiroysl.jpg',
+      'https://i.imgur.com/UPrs1EWl.jpg'
     ],
     indicatorDots: false,
     autoplay: false,
     interval: 5000,
-    duration: 1000
+    duration: 1000,
+    currentIndex: 0
   },
+
   //事件处理函数
-  // bindViewTap: function() {
-  //   wx.navigateTo({
-  //     url: '../logs/logs'
-  //   })
-  // },
-  onLoad: function () {
-    // if (app.globalData.userInfo) {
-    //   this.setData({
-    //     userInfo: app.globalData.userInfo,
-    //     hasUserInfo: true
-    //   })
-    // } else if (this.data.canIUse){
-    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //   // 所以此处加入 callback 以防止这种情况
-    //   app.userInfoReadyCallback = res => {
-    //     this.setData({
-    //       userInfo: res.userInfo,
-    //       hasUserInfo: true
-    //     })
-    //   }
-    // } else {
-    //   // 在没有 open-type=getUserInfo 版本的兼容处理
-    //   wx.getUserInfo({
-    //     success: res => {
-    //       app.globalData.userInfo = res.userInfo
-    //       this.setData({
-    //         userInfo: res.userInfo,
-    //         hasUserInfo: true
-    //       })
-    //     }
-    //   })
-    // }
+  iWantLearn: function() {
+    wx.navigateTo({
+      url: '/pages/cards/flow'
+    })
   },
+
+  /**
+   * 进入页面
+   */
+  onLoad: function () {
+
+    // FIXME, JUST FOR DEVTEST...
+    setTimeout(()=> wx.navigateTo({
+      url: '/pages/cards/flow'
+    }), 100);
+
+    console.log("进入首页");
+    var that = this;
+
+    // check user logged in...to switch tab mine
+    wx.getSetting({success: function (res) {
+      // console.log(res);
+      if (res.authSetting['scope.userInfo']) {
+        wx.checkSession({
+          success: function(){
+
+            var session = Session.get();
+            console.log(session);
+
+            // 如果本地没有回话则重新登录
+            if(!session) return that.switchToMine();
+
+            // that.setData({userInfo: session.userinfo, logged: true});
+          },
+          fail: function () {
+            console.warn('Session is expired!');
+            that.switchToMine();
+          }
+        });
+      } else {
+        // util.showModel('用户未授权', '...');
+        // setTimeout(()=> that.switchToMine(), 100);
+      }
+    }});
+
+  }, // end of onLoad
+
+
+  switchToMine: function () {
+    wx.switchTab({
+      url: '/pages/mine/mine'
+    })
+  },
+
+  // onLoad: function () {
+  //   if (app.globalData.userInfo) {
+  //     this.setData({
+  //       userInfo: app.globalData.userInfo,
+  //       hasUserInfo: true
+  //     })
+  //   } else if (this.data.canIUse){
+  //     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+  //     // 所以此处加入 callback 以防止这种情况
+  //     app.userInfoReadyCallback = res => {
+  //       this.setData({
+  //         userInfo: res.userInfo,
+  //         hasUserInfo: true
+  //       })
+  //     }
+  //   } else {
+  //     // 在没有 open-type=getUserInfo 版本的兼容处理
+  //     wx.getUserInfo({
+  //       success: res => {
+  //         app.globalData.userInfo = res.userInfo
+  //         this.setData({
+  //           userInfo: res.userInfo,
+  //           hasUserInfo: true
+  //         })
+  //       }
+  //     })
+  //   }
+  // },
+
+  onSwiperChange: function(e) {
+    // console.log(e);
+    this.setData({currentIndex: e.detail.current});
+  },
+
+
   getUserInfo: function(e) {
     // console.log(e)
     // app.globalData.userInfo = e.detail.userInfo
@@ -59,7 +120,6 @@ Page({
     //   hasUserInfo: true
     // })
   },
-
 
   onShareAppMessage: function (res) {
     if (res.from === 'button') {
--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,66 +1,254 @@
 // pages/mine/mine.js
+const config = require('../../config');
+const Session = require('../../session');
+const util = require('../../utils/util');
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: '',
+    logo: '/images/logo.jpg',
+    title: '点击登录',
+    selected: 0,
+    prices: [
+      1, 5, 18, 48, 98, 188
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var session = Session.get();
+    console.log(session);
+
+    // 如果本地没有回话则重新登录
+    if(!session) return;
+
+    this.setData({userInfo: session.userinfo, logged: true});
+  },
+
+  // user invode login...
+  bindGetUserInfo: function (e) {
+    if (this.data.logged) return;
+    // cancel selectItem
+    this.setData({ selected: 0 });
+
+    util.showBusy('正在登录');
+
+    var that = this;
+    var userInfo = e.detail.userInfo;
+
+    console.log(userInfo);
+
+    // 查看是否授权
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+
+          // 检查登录是否过期
+          wx.checkSession({
+            success: function () {
+              var session = Session.get();
+              console.log(session);
+              // FIXME, relogin while no session... @2018/05/29
+              // if(!session) that.login();
+              var options = {
+                encryptedData: e.detail.encryptedData,
+                iv: e.detail.iv,
+                userInfo: userInfo
+              }
+              console.log(options);
+
+              if(!session) return that.doLogin(options);
+
+              // 登录态未过期
+              util.showSuccess('登录成功');
+              that.setData({
+                userInfo: userInfo,
+                logged: true
+              });
+            },
+
+            fail: function () {
+              Session.clear();
+              console.warn('checkSession fail to doLogin...');
+              // 登录态已过期，需重新登录
+              var options = {
+                encryptedData: e.detail.encryptedData,
+                iv: e.detail.iv,
+                userInfo: userInfo
+              };
+              console.log(options);
+              that.doLogin(options);
+            },
+          });
+        } else {
+          util.showModel('用户未授权', e.detail.errMsg);
+        }
+      }
+    });
+  },
+
+  doLogin: function (options) {
+    var that = this;
+    console.log('wx login...');
+
+    wx.login({
+      success: function (loginResult) {
+        console.log('loginResult: ');
+        console.log(loginResult);
+        wx.request({
+          url: config.service.loginUrl,
+          method: 'POST',
+          data: {
+            code: loginResult.code ,
+            ...options.userInfo
+          },
+          header: {
+              'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+          },
+          success: function(res) {
+            console.log(res);
+            if(res.data.meta.code == '200'){
+              util.showSuccess('登录成功');
+
+              that.setData({
+                userInfo: options.userInfo,
+                logged: true
+              });
+              var session_3rd = res.data.res.data.session_3rd;
+              // cache userinfo & session...
+              Session.set({userinfo: options.userInfo, session_3rd});
+
+            }else{
+              util.showModel('登录失败', {});
+            }
+          }
+        });
+      },
+      fail: function (loginError) {
+        util.showModel('登录失败', loginError)
+        console.log('登录失败', loginError)
+      },
+    });
+  },
+
+
+  selectItem: function(event) {
+    var total = event.currentTarget.dataset.item;
+    var that = this;
+    that.setData({ selected: total });
+    this.donate(event);
+  },
+
+  donate: function(event) {
+    var that = this;
+
+    var session = Session.get();
+    console.log(session);
+    // 如果本地没有回话则重新登录
+    if(!session) return util.showModel('登录后付款', {info: '需要用微信登录并允许获取用户信息!'});
+
+    util.showBusy('发起付款...');
+    var total = event.currentTarget.dataset.item;
+
+    wx.request({
+      url: config.service.prepayUrl+'/'+ total,
+      method: 'GET',
+      data: {session_3rd: Session.get().session_3rd},// unit: cent
+      header: {
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+      success: function(res) {
+        console.log(res);
+        wx.hideToast();
+
+        if(res.data.meta.code !== 200) return util.showModel('支付失败', {});
+
+        var data = res.data.res.data;
+        console.log(data);
+
+        wx.requestPayment({
+          timeStamp: data.timeStamp,
+          nonceStr: data.nonceStr,
+          package: data.package,
+          signType: 'MD5',
+          paySign: data.paySign,
+          success:function(res){
+            wx.showToast({
+              title: '支付成功,感谢',
+              icon: 'success'
+            });
+          },
+          fail:function(res){
+            that.setData({ selected: 0 });
+            wx.showToast({
+              title: '已取消支付',
+              icon: 'success'
+            });
+          },
+          complete:function(res){
+            // cancel select..
+          }
+        });
+      }
+    });
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
